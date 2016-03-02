58b9712d47d5124efa86ea112f9bcd020cd98d58
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-02 17:49:25 +0100

    starting to look like something usable

--- src/Dbmover.php
+++ src/Dbmover.php
@@ -14,7 +14,7 @@ class Dbmover
 {
     private $pdo;
     private $schemas = [];
-    private $tables = [];
+    private $database;
 
     /**
      * Constructor.
@@ -26,6 +26,8 @@ class Dbmover
      */
     public function __construct($dsn, array $settings = [])
     {
+        preg_match('@dbname=(\w+)@', $dsn, $database);
+        $this->database = $database[1];
         $user = isset($settings['user']) ? $settings['user'] : null;
         $pass = isset($settings['pass']) ? $settings['pass'] : null;
         $options = isset($settings['options']) ? $settings['options'] : [];
@@ -48,8 +50,12 @@ class Dbmover
      */
     public function processSchemas()
     {
-        echo "\033[1;32m"
-            .str_pad('Gathering operations...', 50, ' ', STR_PAD_RIGHT);
+        echo "\033[1;32m".str_pad(
+            "Gathering operations for {$this->database}...",
+            100,
+            ' ',
+            STR_PAD_RIGHT
+        );
         $sql = implode("\n", $this->schemas);
         $operations = [];
         $alter = $this->hoist( '@^ALTER TABLE .*?;$@ms', $sql);
@@ -68,13 +74,43 @@ class Dbmover
             $name = $name[1];
 
             // If the table doesn't exist yet, create it verbatim.
-            if (true) {
+            if (!$this->tableExists($name)) {
                 $operations[] = $table;
-            }
-        }
+            } else {
+                $existing = $this->getTableDefinition($name);
+                $new = $this->parseTableDefinition($table);
+                foreach ($existing as $col => $definition) {
+                    if (!isset($new[$col])) {
+                        $operations[] = sprintf(
+                            "ALTER TABLE %s DROP COLUMN %s",
+                            $name,
+                            $col
+                        );
+                    }
+                }
+                foreach ($new as $col => $definition) {
+                    if (!isset($existing[$col])) {
+                        $operations[] = sprintf(
+                            "ALTER TABLE %s ADD COLUMN %s %s%s%s",
+                            $name,
+                            $col,
+                            $definition['coltype'],
+                            $definition['nullable'] == 'NO' ?
+                                ' NOT NULL' :
+                                '',
+                            $definition['def'] != '' ?
+                                sprintf(
+                                    " DEFAULT %s",
+                                    is_null($definition['def']) ?
+                                        'NULL' :
+                                        $this->pdo->quote($definition['def'])
+                                ) :
+                                ''
+                        );
+                    }
 
-        if (strlen(trim($sql))) {
-            $operations[] = $sql;
+                }
+            }
         }
 
         foreach ($hoists as $hoist) {
@@ -83,12 +119,17 @@ class Dbmover
             $operations[] = $hoist;
         }
 
+        if (strlen(trim($sql))) {
+            $operations[] = $sql;
+        }
+
         // Rerun ifs and alters
         $operations = array_merge($operations, $alter, $ifs);
-        echo "\033[50DPerforming operations...\n";
+        echo "\033[100DPerforming operations for {$this->database}...\n";
 
         echo "\033[0m";
 
+        /*
         $bar = new CliProgressBar(count($operations));
         $bar->display();
 
@@ -108,6 +149,8 @@ class Dbmover
         
         $bar->end();
         echo "\033[0m\n\n";
+        */
+        var_dump($operations);
     }
 
     /**
@@ -130,6 +173,123 @@ class Dbmover
             }
         }
         return $hoisted;
+    }
+
+    /**
+     * Check if a table exists on the given database.
+     *
+     * @param string $name The table name to check.
+     * @return bool True or false.
+     */
+    private function tableExists($name)
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepare("SELECT * FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?");
+        }
+        $stmt->execute([$this->database, $name]);
+        if ($stmt->fetch()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function getTableDefinition($name)
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepare("SELECT
+                COLUMN_NAME name,
+                COLUMN_DEFAULT def,
+                IS_NULLABLE nullable,
+                COLUMN_TYPE coltype,
+                COLUMN_KEY colkey,
+                EXTRA extra
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?
+            ORDER BY ORDINAL_POSITION ASC");
+        }
+        $stmt->execute([$this->database, $name]);
+        $cols = [];
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $column) {
+            $cols[$column['name']] = $column;
+        }
+        return $cols;
+    }
+
+    private function parseTableDefinition($schema)
+    {
+        preg_match("@CREATE TABLE \w+ \((.*)\)@ms", $schema, $extr);
+        $lines = explode(',', $extr[1]);
+        $extra = $cols = [];
+        foreach ($lines as &$line) {
+            $line = trim($line);
+            if (preg_match('@^(CONSTRAINT|UNIQUE|INDEX|PRIMARY)@', $line)) {
+                $extra[] = $line;
+                continue;
+            }
+            $column = [
+                'name' => '',
+                'def' => '',
+                'nullable' => 'YES',
+                'coltype' => '',
+                'colkey' => '',
+                'extra' => '',
+            ];
+            // Extract the name
+            preg_match('@^\w+@', $line, $name);
+            $column['name'] = $name[0];
+            $line = str_replace($name[0], '', $line);
+            if ($this->isNullable($line)) {
+                $column['default'] = null;
+            } else {
+                $column['nullable'] = 'NO';
+            }
+            if ($this->isAutoIncrement($line)) {
+                $column['extra'] = 'auto_increment';
+            }
+            if ($this->isPrimaryKey($line)) {
+                $column['key'] = 'PRI';
+            }
+            $line = preg_replace('@REFERENCES.*?$@', '', $line);
+            $column['coltype'] = strtolower(trim($line));
+            $cols[$name[0]] = $column;
+        }
+        return $cols;
+    }
+
+    private function isNullable(&$column)
+    {
+        if (strpos($column, 'NOT NULL')) {
+            $column = str_replace('NOT NULL', '', $column);
+            return false;
+        }
+        return true;
+    }
+
+    private function isAutoIncrement(&$column)
+    {
+        // MySQL/SQLite
+        if (preg_match('@AUTO_?INCREMENT@', $column)) {
+            $column = preg_replace('@AUTO_?INCREMENT@', '', $column);
+            return true;
+        }
+        // PostgreSQL
+        if (preg_match('@SERIAL@', $column)) {
+            return true;
+        }
+        return false;
+    }
+    
+    private function isPrimaryKey(&$column)
+    {
+        if (preg_match('@(PRIMARY KEY|SERIAL)@', $column)) {
+            $column = str_replace('PRIMARY KEY', '', $column);
+            return true;
+        }
+        return false;
     }
 }
 
