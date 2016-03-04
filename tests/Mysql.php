327455c06c242b3e41390780f630690f7b253e45
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-04 12:40:23 +0100

    remove from test

--- tests/Mysql.php
+++ tests/Mysql.php
@@ -34,9 +34,7 @@ EOT
      * Initially, `test` contains three columns {?}. After we run the migration,
      * there should be four {?}. We should also have a view called `viewtest`
      * {?} and the inserted row has '3' for `bar` since a trigger was created
-     * during the migration {?}. During the migration, we can also call
-     * `dbm_table_exists`, `dbm_column_exists` and `dbm_column_type` and they
-     * return the expected results {?}.
+     * during the migration {?}.
      */
     public function migrations(Dbmover\Mysql &$mysql = null)
     {
