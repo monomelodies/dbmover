e804efdef3c5e807c2c8623a7bd410e107c0a409
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-04 09:33:11 +0100

    make this abstract and fix a bunch of other stuff

--- src/Dbmover.php
+++ src/Dbmover.php
@@ -10,11 +10,16 @@ use Dariuszp\CliProgressBar;
  * The main Dbmover class. This represents a migration for a single unique DSN
  * from the `dbmover.json` config file.
  */
-class Dbmover
+abstract class Dbmover
 {
-    private $pdo;
-    private $schemas = [];
-    private $database;
+    const CATALOG_COLUMN = 'CATALOG';
+    const REGEX_PROCEDURES = '@^CREATE (PROCEDURE|FUNCTION).*?^END;$@ms';
+    const REGEX_TRIGGERS = '@^CREATE TRIGGER.*?^END;$@ms';
+    const DROP_ROUTINE_SUFFIX = '()';
+
+    public $pdo;
+    protected $schemas = [];
+    protected $database;
 
     /**
      * Constructor.
@@ -35,6 +40,16 @@ class Dbmover
     }
 
     /**
+     * Expose the name of the current database.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->database;
+    }
+
+    /**
      * Add schema data read from an SQL file.
      *
      * @param string $schema The complete schema.
@@ -46,32 +61,50 @@ class Dbmover
 
     /**
      * Process all added schemas.
-     *
      */
     public function processSchemas()
     {
-        echo "\033[1;32m".str_pad(
+        echo "\033[0;34m".str_pad(
             "Gathering operations for {$this->database}...",
             100,
             ' ',
             STR_PAD_RIGHT
         );
+        $this->createHelperProcedures();
         $sql = implode("\n", $this->schemas);
         $operations = [];
         $alter = $this->hoist( '@^ALTER TABLE .*?;$@ms', $sql);
-        $ifs = $this->hoist('@^IF.*?^END;$@ms', $sql);
+
+        // Gather all conditionals and optionally wrap them in a "lambda".
+        $ifs = $this->hoist('@^IF.*?^END IF;$@ms', $sql);
+        foreach ($ifs as &$if) {
+            $if = $this->wrapInProcedure($if);
+        }
+
         $operations = array_merge($operations, $alter, $ifs);
         $tables = $this->hoist('@^CREATE TABLE .*?;$@ms', $sql);
 
         // Hoist all other recreatable objects.
-        $hoists = $this->hoist(
-            '@^CREATE (VIEW|PROCEDURE|FUNCTION|TRIGGER).*?^END;$@ms',
-            $sql
+        $hoists = array_merge(
+            $this->hoist(
+                '@^CREATE VIEW.*?;$@ms',
+                $sql
+            ),
+            $this->hoist(
+                static::REGEX_PROCEDURES,
+                $sql
+            ),
+            $this->hoist(
+                static::REGEX_TRIGGERS,
+                $sql
+            )
         );
 
+        $tablenames = [];
         foreach ($tables as $table) {
             preg_match('@^CREATE TABLE (\w+) ?\(@', $table, $name);
             $name = $name[1];
+            $tablenames[] = $name;
 
             // If the table doesn't exist yet, create it verbatim.
             if (!$this->tableExists($name)) {
@@ -90,31 +123,33 @@ class Dbmover
                 }
                 foreach ($new as $col => $definition) {
                     if (!isset($existing[$col])) {
-                        $operations[] = sprintf(
-                            "ALTER TABLE %s ADD COLUMN %s %s%s%s",
-                            $name,
-                            $col,
-                            $definition['coltype'],
-                            $definition['nullable'] == 'NO' ?
-                                ' NOT NULL' :
-                                '',
-                            $definition['def'] != '' ?
-                                sprintf(
-                                    " DEFAULT %s",
-                                    is_null($definition['def']) ?
-                                        'NULL' :
-                                        $this->pdo->quote($definition['def'])
-                                ) :
-                                ''
+                        $operations[] = $this->addColumn($name, $definition);
+                    } else {
+                        $operations = array_merge(
+                            $operations,
+                            $this->alterColumn($name, $definition)
                         );
                     }
-
                 }
             }
         }
 
+        foreach ($this->getTables('VIEW') as $view) {
+            $operations[] = "DROP VIEW $view";
+        }
+        foreach ($this->getRoutines() as $routine) {
+            $operations[] = sprintf(
+                "DROP %s %s%s",
+                $routine['routinetype'],
+                $routine['routinename'],
+                static::DROP_ROUTINE_SUFFIX
+            );
+        }
         foreach ($hoists as $hoist) {
             preg_match('@^CREATE (\w+) (\w+)@', $hoist, $data);
+            if ($data[1] == 'FUNCTION' && $this instanceof Pgsql) {
+                $data[2] .= '()';
+            }
             $operations[] = "DROP {$data[1]} IF EXISTS {$data[2]}";
             $operations[] = $hoist;
         }
@@ -125,11 +160,20 @@ class Dbmover
 
         // Rerun ifs and alters
         $operations = array_merge($operations, $alter, $ifs);
-        echo "\033[100DPerforming operations for {$this->database}...\n";
+
+        // Cleanup: remove stuff that is not in the schema (any more)
+        foreach ($this->getTables('BASE TABLE') as $table) {
+            if (!in_array($table, $tablenames)) {
+                $operations[] = "DROP TABLE $table CASCADE";
+            }
+        }
+        $hoistsql = implode("\n", $hoists);
+
+        // Perform the actual operations and display progress meter
+        echo "\033[100D\033[1;34mPerforming operations for {$this->database}...\n";
 
         echo "\033[0m";
 
-        /*
         $bar = new CliProgressBar(count($operations));
         $bar->display();
 
@@ -148,9 +192,80 @@ class Dbmover
         $bar->display();
         
         $bar->end();
-        echo "\033[0m\n\n";
-        */
-        var_dump($operations);
+        echo "\033[0m";
+    }
+
+    /**
+     * Default implementation for adding a column.
+     *
+     * @param string $table The table to modify.
+     * @param array $definition Key/value hash of column definition.
+     * @return string SQL that adds this column to the table.
+     */
+    protected function addColumn($table, array $definition)
+    {
+        return sprintf(
+            "ALTER TABLE %s ADD COLUMN %s %s%s%s",
+            $table,
+            $definition['name'],
+            $definition['coltype'],
+            $definition['nullable'] == 'NO' ?
+                ' NOT NULL' :
+                '',
+            $definition['def'] != '' ?
+                sprintf(
+                    " DEFAULT %s",
+                    is_null($definition['def']) ?
+                        'NULL' :
+                        $this->pdo->quote($definition['def'])
+                ) :
+                ''
+        );
+    }
+
+    /**
+     * Default implementation for altering a column.
+     *
+     * @param string $table The table to modify.
+     * @param array $definition Key/value hash of column definition.
+     * @return array An array of SQL statements that bring this column into the
+     *  desired state.
+     */
+    protected function alterColumn($table, array $definition)
+    {
+        $operations = [];
+        $base = sprintf(
+            "ALTER TABLE %s ALTER COLUMN %s",
+            $table,
+            $definition['name']
+        );
+        $operations[] = "$base TYPE {$definition['coltype']}";
+        if ($definition['nullable'] == 'NO') {
+            $operations[] = "$base SET NOT NULL";
+        } else {
+            $operations[] = "$base DROP NOT NULL";
+        }
+        if ($definition['def'] != '') {
+            $operations[] = "$base SET DEFAULT "
+                .is_null($definition['def']) ?
+                    'NULL' :
+                    $this->pdo->quote($definition['def']);
+        } else {
+            $operations[] = "$base DROP DEFAULT";
+        }
+        return $operations;
+    }
+
+    /**
+     * Database vendors not allowing direct conditionals (e.g. MySQL) can wrap
+     * them in an "anonymous" procedure ECMAScript-style here.
+     *
+     * @param string $sql The SQL to wrap.
+     * @return string The input SQL potentially wrapped and called.
+     */
+    protected function wrapInProcedure($sql)
+    {
+        return $sql;
     }
 
     /**
@@ -163,7 +278,7 @@ class Dbmover
      * @return array An array of hoisted statements (or an empty array if
      *  nothing matched).
      */
-    private function hoist($regex, &$sql)
+    protected function hoist($regex, &$sql)
     {
         $hoisted = [];
         if (preg_match_all($regex, $sql, $matches, PREG_SET_ORDER)) {
@@ -176,50 +291,89 @@ class Dbmover
     }
 
     /**
+     * Get an array of all table names in this database.
+     *
+     * @return array An array of table names.
+     */
+    protected function getTables($type = 'BASE TABLE')
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepare(sprintf(
+                "SELECT TABLE_NAME
+                    FROM INFORMATION_SCHEMA.TABLES
+                    WHERE TABLE_%s = ? AND TABLE_TYPE = ?",
+                static::CATALOG_COLUMN
+            ));
+        }
+        $stmt->execute([$this->database, $type]);
+        $names = [];
+        var_dump($stmt->queryString);
+        while (false !== ($table = $stmt->fetchColumn())) {
+            var_dump($table);
+            $names[] = $table;
+        }
+        return $names;
+    }
+
+    /**
      * Check if a table exists on the given database.
      *
      * @param string $name The table name to check.
      * @return bool True or false.
      */
-    private function tableExists($name)
+    protected function tableExists($name, $type = 'BASE TABLE')
     {
-        static $stmt;
-        if (!isset($stmt)) {
-            $stmt = $this->pdo->prepare("SELECT * FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?");
-        }
-        $stmt->execute([$this->database, $name]);
-        if ($stmt->fetch()) {
-            return true;
-        } else {
-            return false;
-        }
+        $tables = $this->getTables($type);
+        return in_array($name, $tables);
     }
 
-    private function getTableDefinition($name)
+    /**
+     * Get the relevant parts of a table schema from INFORMATION_SCHEMA in a
+     * vendor-independent way.
+     *
+     * @param string $name The name of the table.
+     * @return array A hash of columns, where the key is also the column name.
+     */
+    protected function getTableDefinition($name)
     {
         static $stmt;
         if (!isset($stmt)) {
-            $stmt = $this->pdo->prepare("SELECT
-                COLUMN_NAME name,
-                COLUMN_DEFAULT def,
-                IS_NULLABLE nullable,
-                COLUMN_TYPE coltype,
-                COLUMN_KEY colkey,
-                EXTRA extra
-            FROM INFORMATION_SCHEMA.COLUMNS
-            WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?
-            ORDER BY ORDINAL_POSITION ASC");
+            $stmt = $this->pdo->prepare(sprintf(
+                "SELECT
+                    COLUMN_NAME name,
+                    COLUMN_DEFAULT def,
+                    IS_NULLABLE nullable,
+                    COLUMN_TYPE coltype,
+                    COLUMN_KEY colkey,
+                    EXTRA extra
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_%s = ? AND TABLE_NAME = ?
+                ORDER BY ORDINAL_POSITION ASC",
+                static::CATALOG_COLUMN
+            ));
         }
         $stmt->execute([$this->database, $name]);
         $cols = [];
         foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $column) {
+            if (is_null($column['def'])) {
+                $column['def'] = 'NULL';
+            } else {
+                $column['def'] = $this->pdo->quote($column['def']);
+            }
             $cols[$column['name']] = $column;
         }
         return $cols;
     }
 
-    private function parseTableDefinition($schema)
+    /**
+     * Parse the table definition as specified in the schema into a format
+     * similar to what `getTableDefinition` returns.
+     *
+     * @param string $schema The schema for this table (CREATE TABLE .. (...);)
+     * @return array A hash of columns, where the key is also the column name.
+     */
+    protected function parseTableDefinition($schema)
     {
         preg_match("@CREATE TABLE \w+ \((.*)\)@ms", $schema, $extr);
         $lines = explode(',', $extr[1]);
@@ -232,7 +386,7 @@ class Dbmover
             }
             $column = [
                 'name' => '',
-                'def' => '',
+                'def' => null,
                 'nullable' => 'YES',
                 'coltype' => '',
                 'colkey' => '',
@@ -242,9 +396,7 @@ class Dbmover
             preg_match('@^\w+@', $line, $name);
             $column['name'] = $name[0];
             $line = str_replace($name[0], '', $line);
-            if ($this->isNullable($line)) {
-                $column['default'] = null;
-            } else {
+            if (!$this->isNullable($line)) {
                 $column['nullable'] = 'NO';
             }
             if ($this->isAutoIncrement($line)) {
@@ -253,6 +405,9 @@ class Dbmover
             if ($this->isPrimaryKey($line)) {
                 $column['key'] = 'PRI';
             }
+            if ($default = $this->getDefaultValue($line)) {
+                $column['def'] = $default;
+            }
             $line = preg_replace('@REFERENCES.*?$@', '', $line);
             $column['coltype'] = strtolower(trim($line));
             $cols[$name[0]] = $column;
@@ -260,7 +415,29 @@ class Dbmover
         return $cols;
     }
 
-    private function isNullable(&$column)
+    /**
+     * Extract and return the default value for a column, if specified.
+     *
+     * @param string $column The referenced column definition.
+     * @return string|null The default value of the column, if specified. If no
+     *  default was specified, null.
+     */
+    protected function getDefaultValue(&$column)
+    {
+        if (preg_match('@DEFAULT (.*?)($| )@', $column, $default)) {
+            $column = str_replace($default[0], '', $column);
+            return $default[1];
+        }
+        return null;
+    }
+
+    /**
+     * Checks whether a column is nullable.
+     *
+     * @param string $column The referenced column definition.
+     * @return bool
+     */
+    protected function isNullable(&$column)
     {
         if (strpos($column, 'NOT NULL')) {
             $column = str_replace('NOT NULL', '', $column);
@@ -269,27 +446,40 @@ class Dbmover
         return true;
     }
 
-    private function isAutoIncrement(&$column)
-    {
-        // MySQL/SQLite
-        if (preg_match('@AUTO_?INCREMENT@', $column)) {
-            $column = preg_replace('@AUTO_?INCREMENT@', '', $column);
-            return true;
-        }
-        // PostgreSQL
-        if (preg_match('@SERIAL@', $column)) {
-            return true;
-        }
-        return false;
-    }
+    /**
+     * Checks whether a column is an auto_increment column.
+     *
+     * @param string $column The referenced column definition.
+     * @return bool
+     */
+    protected abstract function isAutoIncrement(&$column);
     
-    private function isPrimaryKey(&$column)
+    /**
+     * Checks whether a column is a primary key.
+     *
+     * @param string $column The referenced column definition.
+     * @return bool
+     */
+    protected abstract function isPrimaryKey(&$column);
+
+    /**
+     * Create the helper procedures (dbm_*).
+     */
+    protected abstract function createHelperProcedures();
+
+    protected function getRoutines()
     {
-        if (preg_match('@(PRIMARY KEY|SERIAL)@', $column)) {
-            $column = str_replace('PRIMARY KEY', '', $column);
-            return true;
-        }
-        return false;
+        $stmt = $this->pdo->prepare(sprintf(
+            "SELECT
+                ROUTINE_TYPE routinetype,
+                ROUTINE_NAME routinename
+            FROM INFORMATION_SCHEMA.ROUTINES WHERE
+                ROUTINE_%s = '%s'",
+            static::CATALOG_COLUMN,
+            $this->database
+        ));
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 
